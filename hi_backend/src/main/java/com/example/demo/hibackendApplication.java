04290b53d885434c999c4a1cc4afd74014ac9c5a
Kavia AI <production@kavia.ai>
2026-01-28 09:30:31 +0000

    Checkpoint 1 - cgfa9cff3f

---
+++ hi_backend/src/main/java/com/example/demo/hibackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class hibackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(hibackendApplication.class, args);
+	}
+
+}
